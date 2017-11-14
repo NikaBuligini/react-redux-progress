d6d84e0577a80a2c35e7ab9014d1c3d16734b834
Nikoloz Buligini <nbuligini11@gmail.com>
2017-11-14 15:38:49 +0400

    add reducer tests

--- scripts/start-test.js
+++ scripts/start-test.js
@@ -1,7 +1,7 @@
 require('babel-core/register');
 
 // Do this as the first thing so that any code reading it knows the right env.
-process.env.BABEL_ENV = 'test';
+process.env.BABEL_ENV = 'cjs';
 process.env.NODE_ENV = 'test';
 
 module.exports = require('./test.js');
