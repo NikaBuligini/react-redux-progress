e06d0a131a47d57682e55649e3684da03c78ae14
Nikoloz Buligini <nbuligini11@gmail.com>
2023-05-12 08:05:38 +0000

    upgrade packages

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,5 @@
 import path from 'path';
-import babel from '@rollup/plugin-babel';
+import { babel } from '@rollup/plugin-babel';
 import resolve from '@rollup/plugin-node-resolve';
 import typescript from '@rollup/plugin-typescript';
 import esbuild from 'rollup-plugin-esbuild';
